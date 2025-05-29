c689d4d447ff3c491c4742168314ea9b46aafe94
Amirreza Jafarian <amirreza.jfn2000@gmail.com>
2025-05-29 05:57:31 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import HeroSection from './components/HeroSection';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
+
+
