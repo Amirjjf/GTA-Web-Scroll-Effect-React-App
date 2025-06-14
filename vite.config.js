87bfc6b4f16e6f18a97a2305095aa9c06706ae67
Amirreza Jafarian <amirreza.jfn2000@gmail.com>
2025-06-15 01:14:54 +0300

    added scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GTA-Web-Scroll-Effect-React-App/', 
+  base: process.env.NODE_ENV === 'production'
+    ? '/GTA-Web-Scroll-Effect-React-App/'
+    : '/',
   plugins: [react()],
 })
